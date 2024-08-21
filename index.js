851c24a4b3ddd6c37554f140ccb2bcae51310c13
Didar Salimgerey <didarsalimgerey@gmail.com>
2024-08-21 17:29:48 +0500

    feat(sqlite3): add scope of actions

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict'
 
+// TODO: implement user invites
+
 import {createServer} from 'node:http'
 import express from 'express'
 import sqlite3 from 'sqlite3'
 import morgan from 'morgan'
 import cors from 'cors'
 import {actionMsgTemplates} from './templates.js'
+import helmet from 'helmet'
 
 sqlite3.verbose()
 
@@ -24,6 +27,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan())
 app.use(cors())
+app.use(helmet())
+
+const action_scope = { LOCAL: 'local', PROJECT: 'project' }
 
 app.get('/api/v0/todos', async (req, res) => {
   try {
@@ -72,10 +78,18 @@ app.post('/api/v0/todos', async (req, res) => {
   }
 })
 app.post('/api/v0/projects', async (req, res) => {
-
+  try {
+    await new Promise((res, rej) => {
+      db.run(`insert into projects (title, created_by_user_id) values (?, ?)`)
+    })
+  } catch (err) {
+    console.error(err)
+  }
 })
 async function main() {
   const users = [{email: 'adam@gmail.com', password: 'qwert'}, {email: 'sam@gmail.com', password: 'yuiop'}]
+  // TODO: rewrite this pice of code as separate function that
+  // replace placeholders in action message templates
   const createActionMsg = actionMsgTemplates['create']
   const values = ['alex', 'todo', 'create user actions']
   let index = 0
@@ -102,13 +116,34 @@ async function main() {
         foreign key (created_by_user_id) references users (id),
         foreign key (project_id) references projects (id)
       )`)
+      // UNCLEAR: how notifications will be work?
+      // QUESTION: what things must have trigger actions? What main thing that action is belongs?
+      // NOTE: i think will be good if i have two types of actions: 'global' and 'local'.
+      //       'local' actions belongs to each todo
+      //       'global' actions is action records of todos in projects
       db.run(`create table if not exists actions (
         id integer primary key not null,
         action_type text not null,
         created_by_user_id integer not null,
+        todo_id integer not null,
+        scope text check(scope in ('local', 'project')) not null default 'local',
         created_at text not null default current_timestamp
+        foreign key (created_by_user_id) references users (id),
+        foreign key (todo_id) references todos (id)
       )`)
-      db.run(`create table if not exists projects (id integer primary key not null, title text, created_by_user_id integer not null, created_at text not null default current_timestamp)`)
+      // TODO: create table 'reactions' 
+      // NOTE: reactions i think should be on comments only
+      // TODO: create table 'roles'
+      // TODO: create table 'tags' or 'labels'
+      // TODO: create 'invites' table
+      // TODO: create 'members' table
+      db.run(`create table if not exists projects (
+        id integer primary key not null,
+        title text,
+        created_by_user_id integer not null,
+        created_at text not null default current_timestamp,
+        updated_at text not null default current_timestamp
+      )`)
       db.run(`create table if not exists user_projects (user_id integer not null, project_id integer not null, foreign key (user_id) references users (id), foreign key (project_id) references projects (id))`)
       const usersStmt = db.prepare(`insert into users (email, password) values(?, ?)`)
       users.forEach((user) => usersStmt.run(...Object.values(user)))
